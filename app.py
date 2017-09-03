ba7a92cfdd55eaedf96db43ad87d5ad8e68152c2
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-09-03 12:16:27 -0600

    add the comment to python file

--- app.py
+++ app.py
@@ -19,144 +19,48 @@ thread = None
 thread_lock = Lock()
 
 
-# def background_thread():
-#     """Example of how to send server generated events to clients."""
-#     count = 0
-#     while True:
-#         socketio.sleep(10)
-#         count += 1
-#         socketio.emit('my_response',
-#                       {'data': 'Server generated event', 'count': count},
-#                       namespace='/test')
-
-
-
-
-
-
-
-
+#random generate a room number for each study room
 @app.route('/')
 def index():
-    number=str(random.randrange(1000,1000000000000000))
-
+    maximum =1000000000000000
+    minimum=100000
+    number=str(random.randrange(minimum,maximum))
 
     return redirect("/"+number, code=302)
 
 
-
-@app.route('/redirect_to_room',methods=["POST"])
-def re_direct():
-    message=request.form.to_dict()
-    url=message["url"] +message["room"]
-
-
-    return url
-
-
-
-
+#direct user to repective room based on the roomnumebr
 @app.route('/<roomnumber>')
 def rommnum(roomnumber):
-
     session['room']=roomnumber
     return render_template('timer.html', async_mode=socketio.async_mode)
 
-
+#put the newly joint user into the respective room
 @socketio.on("create_new",namespace="/test")
 def new():
     if 'room' in session:
         emit('timer_syn',{'room':session['room']},room=session['room'])
-
         join_room(session['room'])
 
-
-
-
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
+#synchronize the roomnumber and the timer between users
 @socketio.on('room_and_time', namespace='/test')
 def assign_time(message):
     emit('timer_syn',{'room':message['room']},room=message['room'])
     join_room(message['room'])
 
-
+#get the time from one user and synchronize that
 @socketio.on('gettime',namespace='/test')
 def joingroup(message):
     emit('start_timer',{'currenttime':message['currenttime'],'session':message['session'],'pause':message["pause"],'online':message["online"]},room=message['room'])
     print(message["pause"])
 
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
-
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
-
-
+#when new user connect, sent a message to the javascript
 @socketio.on('connect', namespace='/test')
 def test_connect():
     print('room'+session["room"])
     emit('new_connection',{'event':"new coming", "room":session["room"]},room=session["room"])
-    # global thread
-    # with thread_lock:
-    #     if thread is None:
-    #         thread = socketio.start_background_task(target=background_thread)
-    # emit('my_response', {'data': 'Connected', 'count': 0})
 
-
+#called when user disconnect from the room
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print(session["room"])
@@ -164,4 +68,4 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
