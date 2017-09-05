bc29effa66e56e37b41f88170efffabc8fd1d225
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-09-04 19:14:57 -0600

    spacing

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import random
 # the best option based on installed packages.
 async_mode = None
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, async_mode = async_mode)
 thread = None
 thread_lock = Lock()
 
@@ -21,49 +21,48 @@ thread_lock = Lock()
 #random generate a room number for each study room
 @app.route('/')
 def index():
-    maximum =1000000000000000
-    minimum=100000
-    number=str(random.randrange(minimum,maximum))
-
+    maximum = 1000000000000000
+    minimum = 100000
+    number = str(random.randrange(minimum,maximum))
     return redirect("/"+number, code=302)
 
 
 #direct user to repective room based on the roomnumebr
 @app.route('/<roomnumber>')
 def rommnum(roomnumber):
-    session['room']=roomnumber
+    session['room'] = roomnumber
     return render_template('timer.html', async_mode=socketio.async_mode)
 
 #put the newly joint user into the respective room
-@socketio.on("create_new",namespace="/test")
+@socketio.on("create_new",namespace = "/test")
 def new():
     if 'room' in session:
-        emit('timer_syn',{'room':session['room']},room=session['room'])
+        emit('timer_syn',{'room':session['room']},room = session['room'])
         join_room(session['room'])
 
 #synchronize the roomnumber and the timer between users
-@socketio.on('room_and_time', namespace='/test')
+@socketio.on('room_and_time', namespace = '/test')
 def assign_time(message):
-    emit('timer_syn',{'room':message['room']},room=message['room'])
+    emit('timer_syn',{'room':message['room']},room = message['room'])
     join_room(message['room'])
 
 #get the time from one user and synchronize that
-@socketio.on('gettime',namespace='/test')
+@socketio.on('gettime',namespace = '/test')
 def joingroup(message):
-    emit('start_timer',{'currenttime':message['currenttime'],'session':message['session'],'pause':message["pause"],'online':message["online"]},room=message['room'])
+    emit('start_timer',{'currenttime':message['currenttime'],'session':message['session'],'pause':message["pause"],'online':message["online"]},room = message['room'])
     print(message["pause"])
 
 #when new user connect, sent a message to the javascript
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace = '/test')
 def test_connect():
     print('room'+session["room"])
-    emit('new_connection',{'event':"new coming", "room":session["room"]},room=session["room"])
+    emit('new_connection',{'event':"new coming", "room":session["room"]},room = session["room"])
 
 #called when user disconnect from the room
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace = '/test')
 def test_disconnect():
     print(session["room"])
-    emit('disconnection', {'event':"disconnect","room":session["room"]},room=session["room"])
+    emit('disconnection', {'event':"disconnect","room":session["room"]},room = session["room"])
 
 
 if __name__ == '__main__':
